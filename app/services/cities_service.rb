dbcc73f8ad25fe66915feb695355e4fc129269be
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-03 16:31:09 -0700

    add rubocop.yml and correct infractions

--- app/services/cities_service.rb
+++ app/services/cities_service.rb
@@ -10,9 +10,9 @@ class CitiesService
     private
 
     def conn
-      @conn ||= Faraday.new("https://wft-geo-db.p.rapidapi.com/v1/geo/cities") do |req|
+      @conn ||= Faraday.new('https://wft-geo-db.p.rapidapi.com/v1/geo/cities') do |req|
         req.headers['x-rapidapi-key'] = ENV['CITIES_API_KEY']
-        req.params['minPopulation'] = 300000
+        req.params['minPopulation'] = 300_000
         req.params['radius'] = 100
         req.params['types'] = 'CITY'
         req.params['limit'] = 1
