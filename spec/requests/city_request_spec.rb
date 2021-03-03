c0c6cb14be06968bfb7b1e243038c67308d137ad
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-02 21:25:44 -0700

    add test for endpoint response

---
+++ spec/requests/city_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'city request' do
+  it 'returns a JSON response with the name of the nearest large city' do
+    VCR.use_cassette('denver') do
+      get '/api/v1/city', params: {coordinates: '39.7526184-105.0249216'}
+
+      expect(last_response).to eq 200
+      expect(last_response.header['Content-Type']).to eq('application/json')
+      response = JSON.parse(last_response.body, symbolize_names: true)
+
+      expect(response).to be_a(Hash)
+      check_hash_structure(response, :data, Hash)
+      check_hash_structure(response[:data], :id, NilClass)
+      check_hash_structure(response[:data], :type, String)
+      expect(response[:data][:type]).to eq('city')
+      check_hash_structure(response[:data], :attributes, Hash)
+      check_hash_structure(response[:data][:attributes], :name, Hash)
+      check_hash_structure(response[:data][:attributes], :state, Hash)
+      expect(response.keys).to match_array(%i[data])
+      expect(response[:data].keys).to match_array(%i[id type attributes])
+      expect(response[:data][:attributes].keys).to match_array(%i[name state])
+    end
+  end
+end
